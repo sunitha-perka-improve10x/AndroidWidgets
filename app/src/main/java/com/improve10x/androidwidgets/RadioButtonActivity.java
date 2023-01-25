00da72694e1ad2eadbf6b7dedfeef1790fa02006
sunitha <sunithapraveenkumar@gmail.com>
2023-01-25 18:20:53 +0530

    1.completed RadioButtonActivity xml design

--- app/src/main/java/com/improve10x/androidwidgets/RadioButtonActivity.java
+++ app/src/main/java/com/improve10x/androidwidgets/RadioButtonActivity.java
@@ -10,5 +10,6 @@ public class RadioButtonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radio_button);
+        getSupportActionBar().setTitle("Radio Button");
     }
 }
