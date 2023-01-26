ee9245b5f122e588620ce51b85fc7311c1318032
sunitha <sunithapraveenkumar@gmail.com>
2023-01-27 00:39:27 +0530

    created VerticalScrollViewActivity

---
+++ app/src/main/java/com/improve10x/androidwidgets/VerticalScrollViewActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.androidwidgets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerticalScrollViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vertical_scroll_view);
+    }
+}
