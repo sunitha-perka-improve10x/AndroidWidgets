01bb43a8c378385a1cbf8345282f935af89310a6
sunitha <sunithapraveenkumar@gmail.com>
2023-01-26 23:53:21 +0530

    i went to xmlDesign code write <TimePicker give "match_parent" to Width & height.Generate the code

--- app/src/main/java/com/improve10x/androidwidgets/TimePickerActivity.java
+++ app/src/main/java/com/improve10x/androidwidgets/TimePickerActivity.java
@@ -1,14 +1,23 @@
 package com.improve10x.androidwidgets;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.improve10x.androidwidgets.databinding.ActivityTimePickerBinding;
+
 public class TimePickerActivity extends AppCompatActivity {
+
+    ActivityTimePickerBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityTimePickerBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_time_picker);
+        handleTime();
+    }
+
+    private void handleTime() {
+
     }
 }
