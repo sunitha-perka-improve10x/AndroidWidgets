8ab749833f3d0c9bf79fb37b9540fb959c6c8358
sunitha <sunithapraveenkumar@gmail.com>
2023-01-26 16:15:09 +0530

    created DatePickerActivity completed xml Design

---
+++ app/src/main/java/com/improve10x/androidwidgets/DatePickerActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.androidwidgets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatePickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_picker);
+    }
+}
