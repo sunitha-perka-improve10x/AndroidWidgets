6e73bf755fd6ee26170eaa78c7d863ec346807d6
sunitha <sunithapraveenkumar@gmail.com>
2023-01-26 12:20:33 +0530

    Created handleButton method binding is Using thisActivity show the Toast

--- app/src/main/java/com/improve10x/androidwidgets/RatingBarActivity.java
+++ app/src/main/java/com/improve10x/androidwidgets/RatingBarActivity.java
@@ -2,13 +2,29 @@ package com.improve10x.androidwidgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.improve10x.androidwidgets.databinding.ActivityRatingBarBinding;
 
 public class RatingBarActivity extends AppCompatActivity {
+
+    ActivityRatingBarBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rating_bar);
+        binding = ActivityRatingBarBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().setTitle("Ratingbar");
+        handleButton();
+    }
+
+    private void handleButton() {
+        binding.submitBtn.setOnClickListener(view -> {
+            String rating = String.valueOf(binding.ratingbarRb.getRating());
+            Toast.makeText(this, rating, Toast.LENGTH_LONG).show();
+        });
     }
 }
