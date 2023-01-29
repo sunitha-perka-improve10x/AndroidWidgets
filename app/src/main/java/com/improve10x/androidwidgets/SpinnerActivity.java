7ff95847dc6258c129893b6f5bea79911d48c57f
sunitha <sunithapraveenkumar@gmail.com>
2023-01-30 01:53:18 +0530

    created spinner cities method  use the binding setAdapter,set the Data

--- app/src/main/java/com/improve10x/androidwidgets/SpinnerActivity.java
+++ app/src/main/java/com/improve10x/androidwidgets/SpinnerActivity.java
@@ -3,12 +3,60 @@ package com.improve10x.androidwidgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.improve10x.androidwidgets.databinding.ActivitySpinnerBinding;
+
+import java.util.ArrayList;
 
 public class SpinnerActivity extends AppCompatActivity {
+
+    private ActivitySpinnerBinding binding;
+    private ArrayList<String> cities;
+    private Spinner citiesSp;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spinner);
+        binding = ActivitySpinnerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setData();
+        spinnerCities();
+    }
+
+    private void spinnerCities() {
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cities);
+        binding.spinnerSp.setAdapter(arrayAdapter);
+        binding.spinnerSp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(SpinnerActivity.this, cities.get(position), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+
+    private void setData() {
+        cities = new ArrayList<>();
+        cities.add("Hyderabad");
+        cities.add("Delhi");
+        cities.add("Mumbai");
+        cities.add("Kolkata");
+        cities.add("Bengaluru");
+        cities.add("Pune");
+        cities.add("Surat");
+        cities.add("Amritsar");
+        cities.add("Bhopal");
     }
 }
