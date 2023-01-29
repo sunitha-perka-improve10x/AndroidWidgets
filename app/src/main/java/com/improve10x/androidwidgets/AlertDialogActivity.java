f277bc673397e27cdd5a99b6d993901d4496c791
sunitha <sunithapraveenkumar@gmail.com>
2023-01-29 23:08:31 +0530

    created the showAlertDialog method and show the Toast

--- app/src/main/java/com/improve10x/androidwidgets/AlertDialogActivity.java
+++ app/src/main/java/com/improve10x/androidwidgets/AlertDialogActivity.java
@@ -1,8 +1,12 @@
 package com.improve10x.androidwidgets;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.improve10x.androidwidgets.databinding.ActivityAlertDialogBinding;
 
@@ -13,7 +17,21 @@ public class AlertDialogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAlertDialogBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_alert_dialog);
+        setContentView(binding.getRoot());
         getSupportActionBar().setTitle("AlertDialog");
+        showAlertDialog();
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Android Studio");
+        alert.setMessage("Do you love Android ?");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(AlertDialogActivity.this, "Good Job", Toast.LENGTH_SHORT).show();
+            }
+        });
+        alert.create().show();
     }
 }
