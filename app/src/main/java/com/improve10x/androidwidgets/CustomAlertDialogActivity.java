8062f7995fc36896f2bcb3252e50818c0ac51467
sunitha <sunithapraveenkumar@gmail.com>
2023-01-30 17:08:18 +0530

    created CustomAlertDialogActivity design xml completed

--- app/src/main/java/com/improve10x/androidwidgets/CustomAlertDialogActivity.java
+++ app/src/main/java/com/improve10x/androidwidgets/CustomAlertDialogActivity.java
@@ -2,13 +2,34 @@ package com.improve10x.androidwidgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
+import com.improve10x.androidwidgets.databinding.ActivityCustomAlertDialogAcivityBinding;
+import com.improve10x.androidwidgets.databinding.CustomAlertDialogItemBinding;
+
 public class CustomAlertDialogActivity extends AppCompatActivity {
+
+    private ActivityCustomAlertDialogAcivityBinding binding;
+    private Dialog dialog;
+    private CustomAlertDialogItemBinding customAlertDialogIemBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_custom_alert_dialog_acivity);
+        getSupportActionBar().setTitle("Custom Alert Dialog");
+        binding = ActivityCustomAlertDialogAcivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        handleAlertClick();
+
+    }
+
+    private void handleAlertClick() {
+        binding.alertBtn.setOnClickListener(view -> {
+            dialog = new Dialog(this);
+            customAlertDialogIemBinding = CustomAlertDialogItemBinding.inflate(getLayoutInflater());
+            dialog.setContentView(customAlertDialogIemBinding.getRoot());
+        });
+
     }
 }
